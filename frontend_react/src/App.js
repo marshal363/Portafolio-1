81db255f3397e36797f15f7ee00a12634d3c0c7d
Sergio Ardila <sergioandres.363@gmail.com>
2023-03-28 23:17:50 -0400

    3. File and folder structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -24,8 +24,22 @@ function App() {
 
 import React from "react";
 
+import { About, Footer, Header, Skills, Testimonials, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
+
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <Navbar></Navbar>
+      <Header></Header>
+      <About></About>
+      <Work></Work>
+      <Skills></Skills>
+      <Testimonials></Testimonials>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default App;
