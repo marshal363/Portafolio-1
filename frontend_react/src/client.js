d8d42eb661da506302fe7ba13592bc4a0b585bf5
Sergio Ardila <sergioandres.363@gmail.com>
2023-03-31 07:22:50 -0400

    7. Sanity Api set up complete

---
+++ frontend_react/src/client.js
@@ -0,0 +1,16 @@
+/* --Sanity API set up---*/
+
+import sanityClient from "@saniti/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = sanityClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2021-10-21",
+  useCdn: "true",
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
